92115441584989f1913678c423e3ec9b35c6e138
YilinTay1 <yilinTay1@gmail.com>
2022-03-13 02:14:32 +0800

    Final_Version

--- chardevclient.c
+++ chardevclient.c
@@ -1,41 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<fcntl.h>
-#include<string.h>
-#include<unistd.h>
+#include <stdio.h>
 
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
+#include <stdlib.h>
 
-int main(){
-   int ret, fd;
+#include <errno.h>
+
+#include <fcntl.h>
+
+#include <string.h>
+
+#define BUFFER_LENGTH 256
+
+int main()
+
+{
+
+   int fd, rtn;
+
    char stringToSend[BUFFER_LENGTH];
-   printf("Starting device test code example...\n");
-   fd = open("/dev/ebbchar", O_RDWR);             // Open the device with read/write access
-   if (fd < 0){
-      perror("Failed to open the device...");
-      return errno;
-   }
-   printf("Type in a short string to send to the kernel module:\n");
-   scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
-   printf("Writing message to the device [%s].\n", stringToSend);
-   ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-   if (ret < 0){
-      perror("Failed to write the message to the device.");
-      return errno;
+
+   static char receiveString[BUFFER_LENGTH];
+
+   printf("CSC1007 assignment project - User space application program\n");
+
+   fd = open("/dev/chardev", O_RDWR); /* open the chardev driver
+
+   if (fd == -1) /*if fail to open the driver (the drive is not in kernel) */
+
+   {
+
+      perror("open /dev/chardev");
+
+      exit(EXIT_FAILURE);
    }
 
-   printf("Press ENTER to read back from the device...\n");
+   printf("Type in a sentence/message:\n");
+
+   scanf("%[^\n]%*c", stringToSend); /* all the characters entered as the input, including the spaces, until we hit the enter button */
+
+   rtn = write(fd, stringToSend, strlen(stringToSend));
+
+   if (rtn < 0)
+   {
+
+      perror("Failed to write the message to the device.");
+
+      exit(EXIT_FAILURE);
+   }
+
+   printf("\nPress the ENTER key (Wait for the user keyboard input)\n");
+
    getchar();
 
-   printf("Reading from the device...\n");
-   ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
-   if (ret < 0){
+   printf("Sentences/messages being read back from the device driver\n");
+
+   rtn = read(fd, receiveString, BUFFER_LENGTH);
+
+   if (rtn < 0)
+   {
+
       perror("Failed to read the message from the device.");
-      return errno;
+
+      exit(EXIT_FAILURE);
    }
-   printf("The received message is: [%s]\n", receive);
-   printf("End of the program\n");
-   return 0;
+
+   printf("%s\n", receiveString);
+
+   exit(EXIT_SUCCESS);
 }
